06f032a4c0725a596f9219c8bdc9cf03bc083990
a-niveditha <niveditha6174@gmail.com>
2025-05-22 08:17:07 +0530

    tried all-miniLM: better, got op as 60

--- text_parser.py
+++ text_parser.py
@@ -61,7 +61,7 @@ for sent in doc._.textrank.summary(limit_phrases=2, limit_sentences=2):
 
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-mpnet-base-v2")
+model = SentenceTransformer("all-miniLM-L6-v2")
 
 query_embedding = model.encode("We are seeking a public health consultant with expertise in patient education, preventive medicine, and lifestyle intervention programs. The ideal candidate will have experience developing health promotion initiatives, managing grants, and collaborating with universities or healthcare institutions. Responsibilities include supporting physicians and medical residents, contributing to wellness program development, and providing strategic input on healthcare training, nutrition, and disease prevention. A background in family medicine education, community health, and corporate wellness is preferred")
 passage_embeddings = model.encode(str(sent))
